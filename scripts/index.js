d801ce6aaeacbff1b74df515528e65d9ffbac045
Stepan Goldberg <stepan.goldberg@gmail.com>
2020-06-22 16:59:47 +0200

    review_01

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupExitButton = popup.querySelector('.popup__exit-button');
+const formElement = document.querySelector('.popup__form');
+const popupFieldName = popup.querySelector('.popup__field_name');
+const popupFieldDescription = popup.querySelector('.popup__field_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 
 const popupToggle = function() {
   popup.classList.toggle('popup_opened');
@@ -8,13 +13,6 @@ const popupToggle = function() {
 
 profileEditButton.addEventListener('click', popupToggle);
 popupExitButton.addEventListener('click', popupToggle);
-
-const formElement = document.querySelector('.popup__form');
-
-const popupFieldName = popup.querySelector('.popup__field_name');
-const popupFieldDescription = popup.querySelector('.popup__field_description');
-const profileName = document.querySelector('.profile__name');
-const profileDescription = document.querySelector('.profile__description');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -24,11 +22,5 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-// let profileNameFontSize = profileName.style.fontSize;
-
-// if (profileName.length > 15) {
-//   profileName.style.fontSize = profileNameFontSize - 10;
-// }
 
 
